07cba0236ffd7a7151f9e3a734490ca6b036eac9
ludele <Luddesnek@proton.me>
2024-04-19 12:43:54 +0200

    Added functionality audio, visualization

--- js/scripts/audio.js
+++ js/scripts/audio.js
@@ -1,18 +1,73 @@
-function initializeAudioListening(){
+import * as visualizer from "./visualizer.js"
 
-   let currentAudio = new Audio();
-   const audioCtx = new window.AudioContext();
-   let audioSource = null;
-   let analyzer = null;
+/**
+ * - To start audio-listening of an input device. 
+ *    - Output is not allowed through Web-API
+ * - If the user wants to visualize the Audio they are hearing:
+ *    - Temporary file upload
+ *       - Important for mobile devices
+ *    - Microphone input
+ *       - It is possible to reroute output-device to an input-device.
+ */
+function initializeAudioListening() {
+
+   let currentAudio = new Audio()
+   let AudioContext = new AudioContext()
+   let audioSource = null
+   let initAnalyzer = AudioContext.createAnalyser()
+
+   navigator.mediaDevices.getUserMedia({ audio: true })
+      .then(function (stream) {
+         let source = AudioContext.createMediaStreamSource(stream)
+         source.connect(initAnalyzer)
+         // ?
+         visualizer.generateVisualization()
+      })
+      .catch(function (err) {
+
+      })
+}
+
+function playAudio(buffer, audioContext, initAnalyzer) {
+   const source = audioContext.createBufferSource();
+   source.buffer = buffer;
+
+   // Get the analyzer
+   // const analyzer = audioContext.createAnalyser();
+   source.connect(initAnalyzer);
+   analyser.connect(audioContext.destination);
    
-   currentAudio.play();
-   audioSource = audioCtx.createMediaElementSource(currentAudio);
-   analyzer = audioCtx.createAnalyser();
-   audioSource.connect(analyzer);
-   analyzer.connect(audioCtx.destination);
+   source.start();
+
+   analyzeAudio(initAnalyzer)
+}
+
+function analyzeAudio(analyzer) {
+
 
 }
-function analyzeAudio(){}
-function calculateAmplitude(){}
-function calculateFrequenzy(){}
-function calculateWaveform(){}
+
+function calculateAmplitude() { }
+function calculateFrequenzy() { }
+function calculateWaveform() { }
+
+document.getElementById('audioFile').addEventListener('change', function (event) {
+   const file = event.target.files[0];
+   if (!file) {
+      return;
+   }
+
+   const audioContext = new AudioContext();
+   const reader = new FileReader();
+
+   reader.onload = function (fileEvent) {
+      const arrayBuffer = fileEvent.target.result;
+      audioContext.decodeAudioData(arrayBuffer, function (buffer) {
+         playAudio(buffer, audioContext);
+      }, function (e) {
+         console.log('Error decoding file', e);
+      });
+   };
+
+   reader.readAsArrayBuffer(file);
+});
---
+++ js/scripts/visualizationOptions.js
@@ -0,0 +1,32 @@
+const settings = {
+// Default settings - changeable throughout run-time.
+
+   backgroundColor: 'rgb(0, 0, 0)',
+   barColor: function (barHeight) {
+      return `rgb(${barHeight + 100}, 50, 50)`;
+   },
+   barWidth: 5,
+   barSpacing: 1,
+   heightScale: 0.5,
+   smoothingTimeConstant: 0.85,
+   fftSize: 2048
+};
+
+function changeVisualizationColorScheme() {
+
+}
+function changeAnimationStyle() {
+
+}
+
+function changeComplexityLevel() {
+
+}
+
+function changeSmoothing() {
+
+}
+
+function applyView() {
+
+}
